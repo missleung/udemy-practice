7434a4486dae3a3d46ac3d283a129ca58d1a2faa
Lisa Leung <lisa.leung@stat.ubc.ca>
2019-01-15 20:40:06 -0800

    update

--- Neural Network (Udemy)/Convolutional_Neural_Networks/cnn.py
+++ Neural Network (Udemy)/Convolutional_Neural_Networks/cnn.py
@@ -12,17 +12,36 @@
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+from keras.models import Sequential # to initialize neural network. 
+# Remember there are two ways to set neural networks: sequence of layers or graph
+
+from keras.layers import Conv2D # first step - add convolutional layers to deal with images
+from keras.layers import MaxPooling2D # to add pooling steps
+from keras.layers import Flatten # flattening, where we convert pooling maps into a large vector
+from keras.layers import Dense # use to add our fully connected vector into the NN
+
+# Note: Keras uses a data structure > train/test > cat/dog > list of pictures
+# Note: Because pictures are already categorized in a special structure such that keras 
+# package already knows how to read in the data, we don't need to do data preprocessing,
+# we can go straight to CNN
 
 # Initialising the CNN
-classifier = Sequential()
+classifier = Sequential() # create object of a sequential 
 
 # Step 1 - Convolution
+# Reminder: Image first is coded into pixel values (black and white = 1 and 0). Convolution layer 
+# is a set of feature maps. We run a set of feature detectors (a small 3x3 matrix, ie. the corner 
+# of the mouth on an image of a smiley face) to the image by sliding each feature detector to all 
+# 3x3 squares combinations on the image. Then take the sum element-wise multiplication of the two 
+# matrices together. By the way, it's NOT the cross product so I forgot what it's called. When the
+# "corner of the mouth" detector passes thru the corner of the mouth on the image, it automatically
+# spits out a higher value onto the feature map.
+
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+# add the layer for convolutional layer. For the ANN, we used the Dense function which was used to
+# create the full layer of the NN. 
+# Note: click the space after the function before the first paranthesis ( and press CTRL+I
+# first argument of Conv2D is nb_filter
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
